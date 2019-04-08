7fbe9500ff63dfa72fe681d3e4245e08c1ce3b49
Yiftachsa <yiftachs@post.bgu.ac.il>
2019-04-08 16:34:28 +0300

    Merge branch 'master' of https://github.com/yiftachsa/PartA into Merav
    # Conflicts:
    #	src/algorithms/search/DepthFirstSearch.java
    With Last Version 3_4_Ver1

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(1000, 1000);
+        Maze maze = mg.generate(30, 30);
         SearchableMaze searchableMaze = new SearchableMaze(maze);
         solveProblem(searchableMaze, new BreadthFirstSearch());
         solveProblem(searchableMaze, new DepthFirstSearch());
@@ -21,11 +21,11 @@ public class RunSearchOnMaze {
         Solution solution = searcher.solve(domain);
         System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
                 //Printing Solution Path
-        //System.out.println("Solution path:");
-        //ArrayList<AState> solutionPath = solution.getSolutionPath();
-        //for (int i = 0; i < solutionPath.size(); i++) {
-        //    System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
-        //}
+        System.out.println("Solution path:");
+        ArrayList<AState> solutionPath = solution.getSolutionPath();
+        for (int i = 0; i < solutionPath.size(); i++) {
+            System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
+        }
 
     }
 
