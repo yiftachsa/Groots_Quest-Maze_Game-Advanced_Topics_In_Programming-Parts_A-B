c87f96d6c7ea7db3aa48425749b15a8edd0ce11c
Yiftachsa <yiftachs@post.bgu.ac.il>
2019-05-03 18:45:09 +0300

    Merge branch 'Merav' of https://github.com/yiftachsa/PartA into Yiftach
    # Conflicts:
    #	Ass1.iml

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -1,10 +1,7 @@
 package IO;
 
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,66 +12,81 @@ public class MyCompressorOutputStream extends OutputStream {
 
     private OutputStream out;
 
+    public List<Integer> resultTEMP = new ArrayList<Integer>();
+    public Map<List<Byte>, Integer> dictionaryTEMP = new HashMap<List<Byte>, Integer>();
+
     public MyCompressorOutputStream(OutputStream outputStream) {
         out = outputStream;
     }
 
+
     @Override
     public void write(byte[] bytes) throws IOException {
-        //TODO: append to out a phrase after compression.
-            // Build the dictionary.
-            int dictSize = 0;
-            Map<List<Byte>, Integer> dictionary = new HashMap<List<Byte>, Integer>();
+        // Build the dictionary.
+        int dictSize = 2; //###!!!
+        Map<List<Byte>, Integer> dictionary = new HashMap<List<Byte>, Integer>();
 
-            //for (int i = 0; i < 256; i++)
-            //    dictionary.put("" + (char)i, i);
-            List<Byte> temp=new ArrayList<>();
-            byte initializeByte=0;
-            temp.add(initializeByte);
-            dictionary.put(temp,0);
-            temp=new ArrayList<>();
-            initializeByte=1;
-            temp.add(initializeByte);
-            dictionary.put(temp,1);
-            List<Byte> currentBytes=new ArrayList<Byte>();
-            List<Integer> result = new ArrayList<Integer>();
-            for (byte b : bytes) {
-                List<Byte> currentBytesB = new ArrayList<>(currentBytes); //new byte[currentBytes.length +1];
-                currentBytesB.add(b);
-               // System.arraycopy(currentBytes ,0,currentBytesB,0,currentBytes.length);
-               // currentBytesB[currentBytesB.length-1]=b;
-                if (dictionary.containsKey(currentBytesB))
-                    currentBytes = currentBytesB;
-                else {
-                    if(currentBytes.size()!=0) {
-                        Integer test = dictionary.get(currentBytes);
-                        result.add(dictionary.get(currentBytes));
-                    }
-                    // Add currentBytesB to the dictionary.
-                    dictionary.put(currentBytesB, dictSize++);
-                    currentBytes = new ArrayList<>();
-                    currentBytes.add(b);
-                }
-            }
-            // Output the code for w.
-            if (currentBytes.size()!=0)
+        //for (int i = 0; i < 256; i++)
+        //    dictionary.put("" + (char)i, i);
+        List<Byte> temp=new ArrayList<>();
+        byte initializeByte=0;
+        temp.add(initializeByte);
+        dictionary.put(temp,0);
+        temp=new ArrayList<>();
+        initializeByte=1;
+        temp.add(initializeByte);
+        dictionary.put(temp,1);
+
+        List<Byte> currentBytes=new ArrayList<Byte>();
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (byte b : bytes) {
+            List<Byte> currentBytesB = new ArrayList<>(currentBytes); //new byte[currentBytes.length +1];
+            currentBytesB.add(b);
+            // System.arraycopy(currentBytes ,0,currentBytesB,0,currentBytes.length);
+            // currentBytesB[currentBytesB.length-1]=b;
+            if (dictionary.containsKey(currentBytesB))
+                currentBytes = currentBytesB;
+            else {
+                //if(currentBytes.size()!=0) {
+                Integer test = dictionary.get(currentBytes);
                 result.add(dictionary.get(currentBytes));
-           // return result;
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println(result.toArray()[i]);
+                //}
+                // Add currentBytesB to the dictionary.
+                dictionary.put(currentBytesB, dictSize++);
+                currentBytes = new ArrayList<>();
+                currentBytes.add(b);
+            }
         }
-
+        // Output the code for w.
+        if (currentBytes.size()!=0)
+            result.add(dictionary.get(currentBytes));
+        // return result;
+        System.out.println("\n"+"result: ");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.toArray()[i]);
+        }
+        resultTEMP = result;
+        dictionaryTEMP = dictionary;
     }
 
     public static void main(String[] args) throws IOException {
 
-        byte [] test = {1,0,0};
+        byte [] test = {1,0,0,1,0,0,0,0,1,1};
+        System.out.println("before compression: ");
+        for (int i = 0; i < test.length; i++) {
+            System.out.print(test[i]);
+        }
         OutputStream out = new ByteArrayOutputStream();
-        MyCompressorOutputStream testMain=new MyCompressorOutputStream(out);
-        testMain.write(test);
+        MyCompressorOutputStream testMainCompressor=new MyCompressorOutputStream(out);
+        testMainCompressor.write(test);
+
+        InputStream in = new PipedInputStream();
+        MyDecompressorInputStream testMainDecompressor = new MyDecompressorInputStream(in);
+        testMainDecompressor.read(testMainCompressor.resultTEMP);
     }
 
-        @Override
+    @Override
     public void write(int argByte) throws IOException {
         byte[] bytes = BigInteger.valueOf(argByte).toByteArray();
         write(bytes);
--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -2,6 +2,10 @@ package IO;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyDecompressorInputStream  extends InputStream {
 
@@ -14,5 +18,57 @@ public class MyDecompressorInputStream  extends InputStream {
     @Override
     public int read() throws IOException {
         return 0;
+    }
+
+    public byte[] read(List<Integer> compressed) {
+        // Build the dictionary.
+        int dictSize = 0;
+        Map<Integer,List<Byte>> dictionary = new HashMap<Integer,List<Byte>>();
+
+        List<Byte> temp=new ArrayList<>();
+        byte initializeByte=0;
+        temp.add(initializeByte);
+        dictionary.put(0,temp);
+        temp=new ArrayList<>();
+        initializeByte=1;
+        temp.add(initializeByte);
+        dictionary.put(1,temp);
+
+        Byte firstByte= (byte)(int)compressed.remove(0);
+
+        List<Byte> currentBytes=new ArrayList<Byte>();
+        currentBytes.add(firstByte);
+
+        List<Byte> result =new ArrayList<Byte>();
+        result.add(firstByte);
+
+        for (int i : compressed) {
+            List<Byte> entryBytes = new ArrayList<>();
+            if (dictionary.containsKey(i)) {
+                entryBytes = dictionary.get(i);
+            }else if (i == dictSize){
+                entryBytes = currentBytes;
+                entryBytes.add(currentBytes.get(0));//###??
+            }else {
+                throw new IllegalArgumentException("Bad compressed i: " + i);
+            }
+
+            //result.addAll(entryBytes);
+            result.addAll(entryBytes.subList(1,entryBytes.size()));
+
+            // Add w+entry[0] to the dictionary.
+            List<Byte> dictEntry = new ArrayList<>();
+            dictEntry.addAll(currentBytes);
+            dictEntry.add(entryBytes.get(0));
+            dictionary.put(dictSize++, dictEntry);
+
+            currentBytes = entryBytes;
+        }
+
+        System.out.println("\n" +"after decompression: ");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.toArray()[i]);
+        }
+        return null;
     }
 }
