bc54c883ff67b5ed58f3b3416661857823193a5d
Merav <shmera@post.bgu.ac.il>
2019-05-05 11:11:34 +0300

    alot

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -1,6 +1,8 @@
 package IO;
 
 
+import javafx.util.Pair;
+
 import java.io.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -14,6 +16,7 @@ public class MyCompressorOutputStream extends OutputStream {
 
     public List<Integer> resultTEMP = new ArrayList<Integer>();
     public Map<List<Byte>, Integer> dictionaryTEMP = new HashMap<List<Byte>, Integer>();
+    public List<Pair<Integer,Integer>> resultTEMP2 = new ArrayList<Pair<Integer,Integer>>();
 
     public MyCompressorOutputStream(OutputStream outputStream) {
         out = outputStream;
@@ -23,39 +26,46 @@ public class MyCompressorOutputStream extends OutputStream {
     @Override
     public void write(byte[] bytes) throws IOException {
         // Build the dictionary.
-        int dictSize = 0; //###!!!
+        int dictSize = 1; //###!!!
+        int resultInd=0;
         Map<List<Byte>, Integer> dictionary = new HashMap<List<Byte>, Integer>();
 
-        for (int i = -128; i <= 127; i++) {
+      /*  for (int i = -128; i <= 127; i++) {
             List<Byte> temp=new ArrayList<>();
             byte initializeByte=(byte) i;
             temp.add(initializeByte);
             dictionary.put(temp,dictSize++);
         }
+        */
 
         List<Byte> currentBytes=new ArrayList<Byte>();
-        List<Integer> result = new ArrayList<Integer>();
+        List<Pair<Integer,Integer>> result = new ArrayList<Pair<Integer,Integer>>();
 
         for (byte b : bytes) {
             List<Byte> currentBytesB = new ArrayList<>(currentBytes); //new byte[currentBytes.length +1];
             currentBytesB.add(b);
             // System.arraycopy(currentBytes ,0,currentBytesB,0,currentBytes.length);
             // currentBytesB[currentBytesB.length-1]=b;
-            if (dictionary.containsKey(currentBytesB))
+            if (dictionary.containsKey(currentBytesB)){
                 currentBytes = currentBytesB;
+                resultInd=dictionary.get(currentBytesB);
+            }
             else {
-                //if(currentBytes.size()!=0) {
+                /*if(currentBytes.size()!=0) {
                 Integer test = dictionary.get(currentBytes);
                 result.add(dictionary.get(currentBytes));
-                //}
+                }*/
                 // Add currentBytesB to the dictionary.
                 dictionary.put(currentBytesB, dictSize++);
+                result.add(new Pair<>(resultInd,b & 0xFF));
+                resultInd=0;
                 currentBytes = new ArrayList<>();
-                currentBytes.add(b);
+                //currentBytes.add(b);
             }
         }
+        resultTEMP2=result;
         // Output the code for w.
-        if (currentBytes.size()!=0)
+        /*if (currentBytes.size()!=0)
             result.add(dictionary.get(currentBytes));
         // return result;
         System.out.println("\n"+"after compression: ");
@@ -64,7 +74,9 @@ public class MyCompressorOutputStream extends OutputStream {
         }
         resultTEMP = result;
         dictionaryTEMP = dictionary;
+*/
     }
+
 
     public static void main(String[] args) throws IOException {
 
@@ -79,7 +91,7 @@ public class MyCompressorOutputStream extends OutputStream {
 
         InputStream in = new PipedInputStream();
         MyDecompressorInputStream testMainDecompressor = new MyDecompressorInputStream(in);
-        testMainDecompressor.read(testMainCompressor.resultTEMP);
+        testMainDecompressor.read2(testMainCompressor.resultTEMP2);
     }
 
     @Override
