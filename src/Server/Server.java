284aa117f83ac035fd3492c5582725fdc47562ff
yiftachs <yiftachs@post.bgu.ac.il>
2019-05-05 11:25:40 +0300

    5.5.19

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,67 +1,118 @@
-package Server;
+package IO;
 
-import Server.ServerStrategy.IServerStrategy;
+import javafx.util.Pair;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Server {
-    private int port;
-    private int listeningInterval;
-    private IServerStrategy serverStrategy;
-    private volatile boolean stop;
+public class MyDecompressorInputStream  extends InputStream {
 
-    public Server(int port, int listeningInterval, IServerStrategy serverStrategy) {
-        this.port = port;
-        this.listeningInterval = listeningInterval;
-        this.serverStrategy = serverStrategy;
+    InputStream in;
+
+    public MyDecompressorInputStream(InputStream in) {
+        this.in = in;
     }
 
-    public void start() {
-        new Thread(() -> {
-            runServer();
-        }).start();
+    @Override
+    public int read() throws IOException {
+        return 0;
     }
 
-    private void runServer() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            serverSocket.setSoTimeout(listeningInterval);
-            //LOG.info(String.format("Server starter at %s!", serverSocket));
-            //LOG.info(String.format("Server's Strategy: %s", serverStrategy.getClass().getSimpleName()));
-            //LOG.info("Server is waiting for clients...");
-            while (!stop) {
-                try {
-                    Socket clientSocket = serverSocket.accept(); // blocking call
-                    //LOG.info(String.format("Client excepted: %s", clientSocket));
-                    new Thread(() -> {
-                        handleClient(clientSocket);
-                        //LOG.info(String.format("Finished handle client: %s", clientSocket));
-                    }).start();
-                } catch (SocketTimeoutException e) {
-                    //LOG.debug("Socket Timeout - No clients pending!");
-                }
+    public byte[] read(List<Integer> compressed) {
+        // Build the dictionary.
+        int dictSize = 0;
+        Map<Integer, List<Byte>> dictionary = new HashMap<Integer, List<Byte>>();
+
+        for (int i = -128; i <= 127; i++) {
+            List<Byte> temp = new ArrayList<>();
+            byte initializeByte = (byte) i;
+            temp.add(initializeByte);
+            dictionary.put(dictSize++, temp);
+        }
+        /*
+        List<Byte> temp=new ArrayList<>();
+        byte initializeByte=0;
+        temp.add(initializeByte);
+        dictionary.put(0,temp);
+        temp=new ArrayList<>();
+        initializeByte=1;
+        temp.add(initializeByte);
+        dictionary.put(1,temp);
+        */
+        Byte firstByte = (byte) (int) compressed.remove(0);//:FIXME:!!!
+
+        List<Byte> currentBytes = new ArrayList<Byte>();
+        currentBytes.add(firstByte);
+
+        List<Byte> result = new ArrayList<Byte>();
+        result.add(firstByte);
+
+        for (int i : compressed) {
+            List<Byte> entryBytes = new ArrayList<>();
+            if (dictionary.containsKey(i)) {
+                entryBytes = dictionary.get(i);
+            } else if (i == dictSize) {
+                entryBytes = currentBytes;
+                entryBytes.add(currentBytes.get(0));//###??
+            } else {
+                throw new IllegalArgumentException("Bad compressed i: " + i);
             }
-            serverSocket.close();
-        } catch (IOException e) {
-            //LOG.error("IOException", e);
-        }
-    }
 
-    private void handleClient(Socket clientSocket) {
-        try {
-            //LOG.info(String.format("Handling client with socket: %s", clientSocket.toString()));
-            serverStrategy.serverStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
-            clientSocket.close();
-        } catch (IOException e) {
-            //LOG.error("IOException", e);
-        }
-    }
+            result.addAll(entryBytes);
+            //result.addAll(entryBytes.subList(1,entryBytes.size()));
 
-    public void stop() {
-        //LOG.info("Stopping server..");
-        stop = true;
+            // Add w+entry[0] to the dictionary.
+            List<Byte> dictEntry = new ArrayList<>();
+            dictEntry.addAll(currentBytes);
+            dictEntry.add(entryBytes.get(0));
+            dictionary.put(dictSize++, dictEntry);
+
+            currentBytes = entryBytes;
+        }
+
+        System.out.println("\n" + "after decompression: ");
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.toArray()[i]);
+        }
+        return null;
+    }
+    public byte[] read2(List<Pair<Integer,Integer>> compressed) {
+        // byte [] test = {15,0,0,0,0,1,1,1,1,1,1,1};
+        List<String> temp=new ArrayList<String>();
+        temp.add("NULL");
+        int numberOfByte=0;
+        for (int i = 0; i < compressed.size(); i++) {
+            /*for (int j = 0; j < compressed.get(i).getKey()-1; j++) {
+                int check=temp.get(compressed.get(i).getKey()+j);
+                temp.add(temp.get(compressed.get(i).getKey()+j));
+            }
+            */
+            if(compressed.get(i).getKey()!=0){
+                temp.add(temp.get(compressed.get(i).getKey())+","+compressed.get(i).getValue()+"");
+                numberOfByte=numberOfByte+temp.get(i+1).length()-temp.get(i+1).replace(",","").length()+1;
+            }
+            else
+            {
+                temp.add(compressed.get(i).getValue()+"");
+                numberOfByte++;
+
+            }
+
+        }
+        byte[] test=new byte[numberOfByte];
+        int k = 0;
+        for (int i = 1; i < temp.size() ; i++) {
+            String[] afterSplit=temp.get(i).split(",");
+            for (int j = 0; j < afterSplit.length; j++) {
+                int covertStringToInt=Integer.parseInt(afterSplit[j]);
+                test[k] = (byte) covertStringToInt;
+                k++;
+            }
+        }
+        return test;
     }
 }
